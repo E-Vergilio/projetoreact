fcd8ad8e1e5e4cd9690692a8111f51f26d4ae358
Edilene V. Minakawa <103393406+E-Vergilio@users.noreply.github.com>
2024-05-02 10:57:02 -0300

    Update Relogio.js

--- src/Relogio.js
+++ src/Relogio.js
@@ -1,11 +1,24 @@
-
+import React, { useState, useEffect } from 'react';
 
 function Relogio() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const timerID = setInterval(() => tick(), 1000);
+
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
+
+  function tick() {
+    setDate(new Date());
+  }
+
   return (
     <>
-
-    <h1>Meu Relogio</h1>
-    
+      <h1>Meu Relógio</h1>
+      <h2>{date.toLocaleDateString()} {date.toLocaleTimeString()}</h2>
     </>
   );
 }
